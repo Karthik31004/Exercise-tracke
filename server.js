72d20a9a5191275c8d0e0e13c33de4bf1de530c3
Glitch (hello-express) <none>
2020-10-24 09:08:47 +0000

    🎏🏰 Checkpoint ./server.js:22857801/430

--- server.js
+++ server.js
@@ -12,17 +12,16 @@ const app = express();
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-
+app.use(express.json())
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+app.post("/api.exercise/new-user" , (req, res) => {
+  const username = req.body;
+  res.json({username})
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
