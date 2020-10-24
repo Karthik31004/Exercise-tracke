2a0c913a2f778fe4cea5053980a59efe2ab12163
Glitch (hello-express) <none>
2020-10-24 09:31:28 +0000

    🌮👨‍👨‍👦 Checkpoint ./package.json:22857801/2610 ./server.js:22857801/313

--- server.js
+++ server.js
@@ -5,25 +5,29 @@
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
-
+const cors = require('cors')
 // our default array of dreams
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI , {useNewUrlParser: true , newUserTopology: true})
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api.exercise/new-user" , (req, res) => {
-  const username = req.body;
-  res.send("Hello")
+app.post("/api/exercise/new-user" , (req, res) => {
+  const { username } = req.body;
+  res.json({username})
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
