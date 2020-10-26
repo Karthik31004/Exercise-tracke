c6be9ad024e3bce0dbd7e6f33c845f4fdc265852
Glitch (hello-express) <none>
2020-10-26 11:01:30 +0000

    👖🎤 Checkpoint ./server.js:22857801/1173

--- server.js
+++ server.js
@@ -5,19 +5,57 @@ const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-//Connecti
+//Connecting to the database
 mongoose.connect(process.env.MONGO_URI , {useNewUrlParser: true , useUnifiedTopology: true , useCreateIndex: true})
 mongoose.connection.on('connected' , () => {
   console.log("Connected Successfully")
 })
+
+//middlewares
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false})) 
 app.use(cors())
 
+//Starting page
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+//mongoose schema 
+const exerciseSchema = new mongoose.Schema({
+  description: {type: String, required: true} ,
+  duration: {type: Number, required: true},
+  date: {type: Date, default: Date.now()}
+})
+
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true} ,
+  log: [exerciseSchema]
+})
+
+const User = mongoose.model('User' , userSchema)
+const Exercise = mongoose.model('Exercise' , exerciseSchema)
+
+//api
+
+app.post('/api/exercise/new-user' , (req , res) => {
+  const { username } = req.body;
+  User.find({username} , (err , data) => {
+    if(err) {
+      console.log(err)
+    }
+    if(data)  {
+      res.json({message: "User already exists"})
+    }
+    else
+      {
+        const user = new User({
+          username
+        })
+        user.save().then(saved => { res.json({username: saved.username , _id: saved.})})
+      }
+  })
+})
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
